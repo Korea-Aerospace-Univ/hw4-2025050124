4564035bfa4b1e479a1c4df23d82715cf0f35441
2025050124 <ddd193923@gmail.com>
2026-04-15 16:47:24 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,39 +2,28 @@
 
 int main()
 {
-    int answer;
-    
-    int b;
-    
-    int t=0;
-    
-    scanf("%d",&answer);
-    
-    do{
-        scanf("\n%d",&b);
-        if(answer>b){
-            printf("%d<?\n",b);
-        
-        t=t+1;
-        
+    int answer, b;
+    int t = 0;
+
+    scanf("%d", &answer);
+
+    do {
+        scanf("%d", &b);
+        t++;
+
+        if (answer > b) {
+            printf("%d>?\n", b);
         }
-        else if(answer<b){
-            printf("%d>?\n",b);
-            
-        t=t+1;
-        
+        else if (answer < b) {
+            printf("%d<?\n", b);
         }
-        
-        
-        
-        
-         
-    }while(answer!=b);
-    
-    
-    printf("%d==?\n",b);
-    
-    
-    printf("\n%d",t);
-    
+        else {
+            printf("%d==?\n", b);
+        }
+
+    } while (answer != b);
+
+    printf("시도횟수는%d회\n", t);
+
+    return 0;
 }
