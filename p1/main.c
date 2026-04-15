018819f8128a34dea136e49ecdea67cf38dbc651
2025050124 <ddd193923@gmail.com>
2026-04-15 16:54:30 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,26 +2,26 @@
 
 int main()
 {
-    int answer, b;
+    int answer, b; //answer 와 b와 t를 정수형지정 t는 시도 횟수이고 b는 시도값이다.
     int t = 0;
 
-    scanf("%d", &answer);
+    scanf("%d", &answer); //정답 입력 받기
 
     do {
-        scanf("%d", &b);
+        scanf("%d", &b); //시도횟수 1회 추가
         t++;
 
-        if (answer > b) {
+        if (answer > b) {        //답이 b보다 큰경우
             printf("%d>?\n", b);
         }
-        else if (answer < b) {
+        else if (answer < b) {   //답이 b보다 작은경우
             printf("%d<?\n", b);
         }
-        else {
-            printf("%d==?\n", b);
-        }
+        
 
-    } while (answer != b);
+    } while (answer != b); //b와답이 같은경우
+
+    printf("%d==?\n", b);
 
     printf("시도횟수는%d회\n", t);
 
